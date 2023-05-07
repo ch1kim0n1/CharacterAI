fedbff0d8b71d1c2b914b8080e4a7bb34c4c466d
Vlad K <110909601+ch1kim0n1@users.noreply.github.com>
2023-05-07 12:48:45 -0500

    Update Text-to-Speech.py

--- Text-to-Speech.py
+++ Text-to-Speech.py
@@ -1,4 +1,3 @@
-import pyttsx3
 import gtts
 import os
 
@@ -6,20 +5,7 @@ import os
 user_input = input("What would you like me to say? ")
 
 # Convert the user's input to speech.
-tts = gtts.gTTS(user_input)
-
-# Create a pyttsx3 engine object.
-engine = pyttsx3.init()
-
-engine.setProperty('rate', 150)
-# Set volume 0-1
-engine.setProperty('volume', 0.7)
-
-# Speak text using the engine object.
-engine.say(tts)
-
-# Stop the engine
-engine.stop()
+tts = gtts.gTTS(user_input, lang="en-us-female")
 
 # Save the converted audio in a file.
 tts.save("user_input.mp3")
